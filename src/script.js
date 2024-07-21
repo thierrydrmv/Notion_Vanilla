350b7df070baad3d390a5304cf248186826d75fc
Thierry Varela <varelathierry@gmail.com>
2024-07-21 16:47:49 -0300

    feat: base of the project

---
+++ src/script.js
@@ -0,0 +1,69 @@
+const content = document.getElementById('content')
+const modal = document.getElementById('modal')
+const divs = document.getElementsByTagName('div')
+function input() {
+  const modalN = document.getElementById('modalN')
+  console.log(modalN.value)
+  var tags = Array.from(divs)
+  for (let index = 0; index < tags.length; index++) {
+    if (getData(index)) {
+      if (getData(index).includes('/')) {
+        openModal(index)
+      }
+    }
+  }
+}
+function createH1(index) {
+  removeModal()
+  var text = divs[index].innerText.replace('/1', '')
+  divs[
+    index
+  ].innerHTML = `<h1 contentEditable=true data-text="Heading 1" id="h1">${text}</h1>`
+}
+function getData(index) {
+  return divs[index]?.childNodes[0]?.data
+}
+function openModal(index) {
+  const modal = document.getElementById('modal')
+  var data = getData(index)
+  if (data.includes('/1') && !modal) {
+    content.innerHTML += `
+      <div onkeydown="keyCode(event)" contenteditable="false" id="modal" class="popup">
+        <strong>Add Blocks</strong>
+        <p>Keep typing to filter, or escape to exit</p>
+        <p>Filtering keyword 1️⃣</p>
+        <button id="focus" onClick="createH1(${index})" class="options">
+          <img src="https://super.so/icon/dark/type.svg" alt="" />
+          <div class="block">
+            <strong>Heading 1</strong>
+            <p class="margin">Shortcut: type # + space</p>
+          </div>
+        </button>
+        <button onClick="createH1(${index})" class="options">
+          <img src="https://super.so/icon/dark/type.svg" alt="" />
+          <div class="block">
+            <strong>Expandable Heading 1</strong>
+            <p class="margin">Shortcut: type >># + space</p>
+          </div>
+        </button>
+      </div>
+    `
+    const focus = document.getElementById('focus').focus()
+  }
+}
+function keyCode(event) {
+  if (event.key != 'Enter') {
+    removeModal()
+  } else if (event.key == 'Escape') {
+    modalOff()
+  }
+}
+function removeModal() {
+  const modal = document.getElementById('modal')
+  modal.remove()
+}
+function modalOff() {
+  const modalN = document.getElementById('modalN')
+  modalN.document.value = true
+}
+content.addEventListener('keyup', input)
